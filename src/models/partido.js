f7a4dc827990cd616f7ffdfd707d29f0ef742411
Kelvin Melgar <kelvinmelgar777@gmail.com>
2026-09-05 14:15:08 -0600

    Mini-server listo

--- src/models/partido.js
+++ src/models/partido.js
@@ -1,15 +1,41 @@
 let partidos = [];
 let nextId = 1;
 
-function listar() {}
+function listar() {
+  return partidos;
+}
 
-function obtener(id) {}
+function obtener(id) {
+  return partidos.find((p) => p.id === id);
+}
 
-function crear({ torneoId, equipoLocalId, equipoVisitanteId, arbitroId, sets }) {}
+function crear({ torneoId, equipoLocalId, equipoVisitanteId, arbitroId, sets }) {
+  const partido = {
+    id: nextId++,
+    torneoId,
+    equipoLocalId,
+    equipoVisitanteId,
+    arbitroId,
+    sets: sets || [],
+    estado: "programado",
+  };
+  partidos.push(partido);
+  return partido;
+}
 
-function actualizar(id, datos) {}
+function actualizar(id, datos) {
+  const partido = obtener(id);
+  if (!partido) return null;
+  Object.assign(partido, datos);
+  return partido;
+}
 
-function eliminar(id) {}
+function eliminar(id) {
+  const index = partidos.findIndex((p) => p.id === id);
+  if (index === -1) return false;
+  partidos.splice(index, 1);
+  return true;
+}
 
 module.exports = {
   listar,
